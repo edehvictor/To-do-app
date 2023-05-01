89e8de5b39dce1773d1be7b2cf6b5f7049cc82ca
Victor Edeh <edehvictor715@gmail.com>
2023-05-01 03:39:47 +0100

    Refactor the code

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FaTrashAlt } from 'react-icons/fa'
+// import { FaTrashAlt } from 'react-icons/fa'
 
 const Footer = ({ count, handleDelteCompletedTask }) => {
     // const getdate = new Date()
     return (
         <div className='footer' >
             <h2 > {count} completed  </h2>
-            <span onClick={handleDelteCompletedTask}><FaTrashAlt/></span>
+            <span onClick={handleDelteCompletedTask}>✂</span>
 
         </div>
     )
